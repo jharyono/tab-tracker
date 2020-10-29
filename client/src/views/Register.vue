1bbc0227391acafac3298c746c33327d372c7b62
JONI HARYONO <jharyono@gamil.com>
2020-10-29 10:27:25 +0700

    setting up registration page

---
+++ client/src/views/Register.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="register">
+    <h1>Register</h1>
+    <input type="email" v-model="email" name="email" placeholder="email" />
+    <br />
+    <input
+      type="password"
+      v-model="password"
+      name="password"
+      placeholder="password"
+    />
+    <br />
+    <button @click="register">Register</button>
+  </div>
+</template>
+
+<script>
+import Authentication from '@/services/Authentication'
+export default {
+  data() {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    async register() {
+      await Authentication.register({
+        email: this.email,
+        password: this.password
+      })
+    }
+  }
+}
+</script>
