3269cd45521bd11028ebed7589de3165f572e4b1
JONI HARYONO <jharyono@gamil.com>
2020-10-31 08:06:13 +0700

    Add Vuetify and remodelling register page

--- client/src/views/Register.vue
+++ client/src/views/Register.vue
@@ -1,35 +1,66 @@
 <template>
-  <div class="register">
-    <h1>Register</h1>
-    <input type="email" v-model="email" name="email" placeholder="email" />
-    <br />
-    <input
-      type="password"
-      v-model="password"
-      name="password"
-      placeholder="password"
-    />
-    <br />
-    <button @click="register">Register</button>
-  </div>
+  <v-app>
+    <v-card max-width="500px" width="400" class="mt-5 mx-auto">
+      <v-card-title class="pb-0">
+        <h3>Login</h3>
+      </v-card-title>
+      <v-card-text>
+        <v-form>
+          <v-text-field
+            label="Username"
+            type="email"
+            v-model="email"
+            name="email"
+            prepend-icon="mdi-account-circle"
+          />
+          <v-text-field
+            :type="showPassword ? 'text' : 'password'"
+            label="Password"
+            v-model="password"
+            prepend-icon="mdi-lock"
+            :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+            @click:append="showPassword = !showPassword"
+          />
+        </v-form>
+      </v-card-text>
+      <v-divider></v-divider>
+      <v-card-actions>
+        <v-btn @click="register" color="success">Register</v-btn>
+        <v-spacer></v-spacer>
+        <v-btn color="info">Login</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-app>
 </template>
 
 <script>
-import Authentication from '@/services/Authentication'
+import AuthenticationService from '@/services/AuthenticationService'
 export default {
   data() {
     return {
       email: '',
-      password: ''
+      password: '',
+      error: '',
+      showPassword: false
     }
   },
   methods: {
     async register() {
-      await Authentication.register({
-        email: this.email,
-        password: this.password
-      })
+      try {
+        await AuthenticationService.register({
+          email: this.email,
+          password: this.password
+        })
+      } catch (error) {
+        this.error = error.response.data.error
+      }
     }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.err {
+  color: red;
+}
+</style>
